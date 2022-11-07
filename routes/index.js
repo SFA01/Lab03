d1b8f30828ce04c1d531755736902773943b7f97
SFA01 <sarafranzinoandrade@hotmail.com>
2022-11-07 14:56:33 -0500

    routed added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sara', function(req, res, next) {
+  res.render('sara', { title: 'Sara' });
+});
+
+router.get('/nivaldo', function(req, res, next) {
+  res.render('nivaldo', { title: 'Nivaldo' });
+});
+
+router.get('/simone', function(req, res, next) {
+  res.render('simone', { title: 'Simone' });
+});
+
+
+
+
 module.exports = router;
+
